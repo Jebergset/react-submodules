1e8a2f9a57812a93a3b49890d2c012409a3b99aa
Jbergset <Jbergset93@hotmail.com>
2019-11-27 09:39:29 +0100

    Updates

---
+++ precache-manifest.0282459589770395c99d7ddff7302283.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ebc29937a30cca8d3a226e432f7898a",
+    "url": "/react-submodules/index.html"
+  },
+  {
+    "revision": "665f365e6e79785553dc",
+    "url": "/react-submodules/static/css/main.7040ac40.chunk.css"
+  },
+  {
+    "revision": "da8af002cc14d55d6177",
+    "url": "/react-submodules/static/js/2.a922cbb3.chunk.js"
+  },
+  {
+    "revision": "665f365e6e79785553dc",
+    "url": "/react-submodules/static/js/main.baa6b449.chunk.js"
+  },
+  {
+    "revision": "c900d3900270f3cc22a9",
+    "url": "/react-submodules/static/js/runtime-main.6779e2cf.js"
+  }
+]);
